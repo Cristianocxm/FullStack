ae40eec60682877f6d44f1c64441282dcf809454
Cristianocxm <114105719+Cristianocxm@users.noreply.github.com>
2024-11-04 21:51:28 -0300

    Add files via upload

---
+++ Trabalho 5/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Cadastro from "./components/cadastro";
+import Login from "./components/login";
+
+function App(){
+  return(
+    <div className="App">
+      <Cadastro/>
+    </div>
+  );
+}
+
+export default App;
