e1c53c4ef4ace3bfad3d47a70cc80e8b5c298489
Cristianocxm <114105719+Cristianocxm@users.noreply.github.com>
2024-11-11 21:45:38 -0300

    Add files via upload

---
+++ Trabalho 6/hooks/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Contador from "./components/Contador";
+import ListaDeUsuario from "./components/ListaDeUsuario";
+import TemaEscuro from "./components/TemaEscuro";
+
+function App(){
+  return(
+    <div className="App">
+      <ListaDeUsuario/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Trabalho 6/hooks/src/components/TemaEscuro.js
@@ -0,0 +1,21 @@
+import React, { useState, useContext, createContext } from 'react';
+
+const ThemeContext = createContext();
+
+function TemaEscuro(){
+    const [dark, setDark] = useState(false);
+
+    return(
+        <ThemeContext.Provider value={dark}>
+            <button onClick={()=> setDark(!dark)}>Alterar Tema</button>
+            <Pagina />
+        </ThemeContext.Provider>
+    );
+}
+
+function Pagina(){
+    const dark = useContext(ThemeContext);
+    return <div style={{ background: dark ? '#333': '#fff'}}>Página</div>
+}
+
+export default TemaEscuro;
